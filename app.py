9483af2623b48e468674f160f7a0921b7694d938
AhmedaliElgabry <53542478+AhmedaliElgabry@users.noreply.github.com>
2020-07-13 18:24:07 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask,flash, request, jsonify, render_template,redirect,send_from_directory,session, url_for
+import flask
+from werkzeug.utils import secure_filename
+import pickle
+import requests
+from io import BytesIO
+import spacy
+import glob
+from summarizer import Summarizer
+import numpy as np
+#-------------------------
+
+
+model = Summarizer()
+
+
+
+app = Flask(__name__)
+
+@app.route('/',methods = ['GET','POST'])
+def home():
+    if request.method == 'POST':
+      text = request.form['message']
+      body = text
+      result = model(body, min_length=100)
+      full = ''.join(result)
+      print(full)
+     
+      return redirect(url_for('success', name=full))
+
+    else:
+      return render_template('index.html')
+
+@app.route('/success/<name>')
+def success(name):
+    print('summery: ',name)
+    return render_template('index.html',data = name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
